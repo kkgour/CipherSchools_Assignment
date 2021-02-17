f64d598e09909fdaa2c5fd53efc7f67a21b6659b
kkgour <57001286+kkgour@users.noreply.github.com>
2021-02-17 21:58:15 +0530

    Add files via upload

---
+++ codes/Assignment2/KeyPad.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+string getValue(char ch){
+    switch(ch){
+        case '1':return "abc";
+            break;
+        case '2':return "def";
+            break;
+        case '3':return "ghi";
+            break;
+        default :break;
+    }
+}
+vector<string> getAllComb(string s){
+    if(s.length()==0){
+        vector<string> emptylist;
+        emptylist.push_back("");
+        return emptylist;
+    }
+    string first=getValue(s[0]);
+    vector<string> nextcomb=getAllComb(s.substr(1));
+    vector<string> res;
+
+    for(int i=0;i<first.length();i++){
+        for(int j=0;j<nextcomb.size();j++){
+            string s1=first[i]+nextcomb[j];
+            res.push_back(s1);
+        }
+    }
+    return res;
+}
+int main(){
+    string s;
+    cin>>s;
+    vector<string> v=getAllComb(s);
+    vector<string> :: iterator itr;
+    for(itr=v.begin();itr!=v.end();itr++){
+        cout<<*itr<<endl;
+    }
+
+
+}
